c35ef9a51d1f2409ef802eebec7fee16b4357d65
FreeZe06 <tim.vannson@ynov.com>
2024-03-29 14:58:27 +0100

    up header

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/users', require('./userController'));
+router.use('/users', require('./controllers/userController'));
 router.use('/rides', require('./rideController'));
 router.use('/payments', require('./paymentController'));
 
