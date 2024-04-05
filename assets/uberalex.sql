14f998f578bac10317c8c28da99e5ae0e3da29cc
Fr33Ze06 <118484751+FreeZe060@users.noreply.github.com>
2024-04-05 19:37:32 +0200

    Update uberalex.sql

--- assets/uberalex.sql
+++ assets/uberalex.sql
@@ -16,12 +16,10 @@
 
 
 -- Listage de la structure de la base pour uberalex
-DROP DATABASE IF EXISTS `uberalex`;
 CREATE DATABASE IF NOT EXISTS `uberalex` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `uberalex`;
 
 -- Listage de la structure de table uberalex. admin
-DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
@@ -33,9 +31,9 @@ CREATE TABLE IF NOT EXISTS `admin` (
 DELETE FROM `admin`;
 
 -- Listage de la structure de table uberalex. member
-DROP TABLE IF EXISTS `member`;
 CREATE TABLE IF NOT EXISTS `member` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `img` varchar(50) DEFAULT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
@@ -43,23 +41,23 @@ CREATE TABLE IF NOT EXISTS `member` (
   `address` varchar(100) DEFAULT NULL,
   `balance` decimal(10,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table uberalex.member : ~0 rows (environ)
+-- Listage des données de la table uberalex.member : ~2 rows (environ)
 DELETE FROM `member`;
-INSERT INTO `member` (`id`, `first_name`, `last_name`, `password`, `email`, `address`, `balance`) VALUES
-	(1, 'Alexandre ', 'Perez', '123', 'alex.perezab470@gmail.com', '6 Avenue Maria_95100_ARGENTEUIL', 0.00),
-	(2, 'alex.perezac490@gmail.com', 'Xavier Perez', '123', 'alex.perezac490@gmail.com', '6 avenue maria_95100_argenteuil', 0.00);
+INSERT INTO `member` (`id`, `img`, `first_name`, `last_name`, `password`, `email`, `address`, `balance`) VALUES
+	(1, NULL, 'Alexandre ', 'Perez', '123', 'alex.perezab470@gmail.com', '6 Avenue Maria_95100_ARGENTEUIL', 0.00),
+	(2, NULL, 'alex.perezac490@gmail.com', 'Xavier Perez', '123', 'alex.perezac490@gmail.com', '6 avenue maria_95100_argenteuil', 0.00),
+	(4, NULL, 'Tim', 'Vannson', '123', 'Tim_V@outlook.fr', '10 rue_06000_Nice', 0.00);
 
 -- Listage de la structure de table uberalex. order
-DROP TABLE IF EXISTS `order`;
 CREATE TABLE IF NOT EXISTS `order` (
   `id` int NOT NULL AUTO_INCREMENT,
   `product_id` int NOT NULL,
   `restaurant_id` int NOT NULL,
   `member_id` int NOT NULL,
   `quantity` int NOT NULL DEFAULT '1',
-  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` enum('pending','completed') NOT NULL DEFAULT 'pending',
   PRIMARY KEY (`id`),
   KEY `product_id` (`product_id`),
@@ -74,10 +72,10 @@ CREATE TABLE IF NOT EXISTS `order` (
 DELETE FROM `order`;
 
 -- Listage de la structure de table uberalex. product
-DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
   `id` int NOT NULL AUTO_INCREMENT,
   `restaurant_id` int NOT NULL,
+  `img` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `name` varchar(100) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`),
@@ -89,25 +87,28 @@ CREATE TABLE IF NOT EXISTS `product` (
 DELETE FROM `product`;
 
 -- Listage de la structure de table uberalex. restaurant
-DROP TABLE IF EXISTS `restaurant`;
 CREATE TABLE IF NOT EXISTS `restaurant` (
   `id` int NOT NULL AUTO_INCREMENT,
   `restaurateur_id` int NOT NULL,
+  `img` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'default.jpg',
   `name` varchar(100) NOT NULL,
+  `desc` varchar(50) DEFAULT NULL,
   `address` varchar(100) NOT NULL,
   `rating` decimal(3,2) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `restaurateur_id` (`restaurateur_id`),
   CONSTRAINT `restaurant_ibfk_1` FOREIGN KEY (`restaurateur_id`) REFERENCES `restaurateur` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table uberalex.restaurant : ~0 rows (environ)
 DELETE FROM `restaurant`;
+INSERT INTO `restaurant` (`id`, `restaurateur_id`, `img`, `name`, `desc`, `address`, `rating`) VALUES
+	(1, 1, 'KFC.png', 'KFC', 'Cool Chicken', '6 rue zeub', 4.20);
 
 -- Listage de la structure de table uberalex. restaurateur
-DROP TABLE IF EXISTS `restaurateur`;
 CREATE TABLE IF NOT EXISTS `restaurateur` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `img` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `first_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `last_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` varchar(50) NOT NULL,
@@ -118,8 +119,8 @@ CREATE TABLE IF NOT EXISTS `restaurateur` (
 
 -- Listage des données de la table uberalex.restaurateur : ~1 rows (environ)
 DELETE FROM `restaurateur`;
-INSERT INTO `restaurateur` (`id`, `first_name`, `last_name`, `password`, `email`, `address`) VALUES
-	(1, 'alex.perezab470@gmail.com', 'Alexandre Perez', '123', 'alex.perezab470@gmail.com', '6 Avenue Maria_95100_ARGENTEUIL');
+INSERT INTO `restaurateur` (`id`, `img`, `first_name`, `last_name`, `password`, `email`, `address`) VALUES
+	(1, NULL, 'alex.perezab470@gmail.com', 'Alexandre Perez', '123', 'alex.perezab470@gmail.com', '6 Avenue Maria_95100_ARGENTEUIL');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
