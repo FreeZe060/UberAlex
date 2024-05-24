c2c3838686679fbfe4d6cbe68f24ca9951bbddfa
FreeZe06 <tim.vannson@ynov.com>
2024-05-24 17:00:26 +0200

    up sql

---
+++ models/productModel.js
@@ -0,0 +1,14 @@
+const dbManager = require('../config/dbManager');
+
+class ProductModel {
+    constructor() {
+        this.dbManager = new dbManager();
+    }
+
+    async getAllOrdersByRestaurantId(memberId) {
+    }
+        
+}
+
+
+module.exports = new productModel();
